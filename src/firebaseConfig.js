d9983ffcf2ed1bccd2df0b2ba6a9123e161065f5
ghgfhfg2 <46801546+ghgfhfg2@users.noreply.github.com>
2023-11-25 00:23:17 +0900

    Delete src/firebaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBgPxv2Ir4oz5lYpo4-p6PFguHwr7b0Lsk",
-  authDomain: "sy-hope-sound.firebaseapp.com",
-  projectId: "sy-hope-sound",
-  storageBucket: "sy-hope-sound.appspot.com",
-  messagingSenderId: "658377261284",
-  appId: "1:658377261284:web:a146674877dd6738a96cf1",
-  measurementId: "G-1ZB335JPPJ",
-};
